000205026e239c4302d97a3f7adca3c14e943ad2
guilhermeleroy <leroy.luiz@gmail.com>
2020-10-01 10:06:39 -0300

    Commit/Push 2

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ class Main {
     public static void main(String[] args){
       Tests test = new Tests();
       test.testMultiplication();
+      test.testEquality();
+      test.testFrancMultiplication();
     }
   } 
